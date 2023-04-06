98af5dad21067f3d070a15aa892c15409b31cc41
Adrian Imioło <105633316+adrian-imiolo@users.noreply.github.com>
2023-04-06 23:32:00 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Tenzies from "./Tenzies/Tenzies"
+import './style.css'
+
+
+export default function App() {
+
+    const styles = {
+        backgroundColor: "yellow",
+        color: "black"
+    }
+
+    return (
+        <div className="front-page">
+            <Tenzies />
+        </div>
+
+
+
+
+
+
+
+    )
+
+}
+
